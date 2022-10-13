e33cad3b0fee5d0bc12c04b97d3235374b19bd18
trenzterra <trenzterra@gmail.com>
2022-10-13 09:23:32 +0800

    V1 ISO non-knob

---
+++ keyboards/keychron/v1/v1_iso_stm32l432/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x16, 0x80, 0xC0, 0xB9, 0x6B, 0x66, 0xE8, 0x92}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
